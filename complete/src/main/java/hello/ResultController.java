29d61690d7102a900a9f96b9f896494defb5aee5
马志宇(无线运营中心) <zhiyuma@sohu-inc.com>
2015-05-08 15:41:36 +0800

    实现两种结果的处理

--- complete/src/main/java/hello/ResultController.java
+++ complete/src/main/java/hello/ResultController.java
@@ -1,16 +1,24 @@
 package hello;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class ResultController {
 
+	@RequestMapping("/getResult")
+	public Result getResult(
+			@RequestParam(value = "success", defaultValue = "false") String success) {
+		Result r = new Result();
+		if (StringUtils.isEmpty(success) || success.equals("false")) {
+			r.setStatus(404);
+			r.getData().setStatus(404);
+			return r;
+		} else {
+			return r;
+		}
 
-    @RequestMapping("/getResult")
-    public Result getResult(@RequestParam(value="orderSn", defaultValue="1024") String orderSn) {
-    	return new Result();
-    }
+	}
 }
